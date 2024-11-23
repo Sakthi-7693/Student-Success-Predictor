24e9ed89863ea4705346365a2ba89bb8b3c1a335
Sakthi <sakthi.1617s@gmail.com>
2024-11-24 01:50:11 +0530

    Model-Evaluation

--- src/logger.py
+++ src/logger.py
@@ -2,9 +2,10 @@ import logging
 import os
 from datetime import datetime
 
-Log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",Log_file)
+logs_path = os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path, exist_ok=True)
+
+Log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 Log_file_path = os.path.join(logs_path, Log_file)
 
